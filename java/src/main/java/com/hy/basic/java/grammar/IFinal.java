36b35042e50dab2d9b765770cca352b10c964add
core <602135064@qq.com>
2021-03-10 22:40:30 +0800

    final语法解释

---
+++ java/src/main/java/com/hy/basic/java/grammar/IFinal.java
@@ -0,0 +1,32 @@
+package com.hy.basic.java.grammar;
+
+/**
+ *
+ *  final
+ */
+public class IFinal {
+
+
+    private final String str1 = null;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void explain() {
+
+    }
+
+    public static void question() {
+
+        /*
+            1.String为什么是被final修饰的?
+                a.为了实现字符串池,主要是安全性(和private一起让字符不可变)和效率          //https://blog.csdn.net/qq_39581763/article/details/104690625
+                b.为了线程安全,不会因为安全
+                c.为了实现String可以创建HashCode不可变性
+
+         */
+
+    }
+
+}
